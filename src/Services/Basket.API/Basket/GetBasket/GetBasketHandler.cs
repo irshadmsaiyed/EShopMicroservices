745a9aff3dfd36cea99cb79cb41a59ce47dc8fed
irshadmsaiyed <irshadmsaiyed@gmail.com>
2026-01-07 20:50:49 +0400

    implement custom exception handler in basket service

--- src/Services/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -4,12 +4,11 @@ public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
 
 public record GetBasketResult(ShoppingCart Cart);
 
-public class GetBasketHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+public class GetBasketHandler(IBasketRepository repository) : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
     public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
     {
-        // TODO: get basket from database
-        // var basket = await _repository.GetBasket(query.UserName);
-        return new GetBasketResult(new ShoppingCart("swn"));
+        var basket = await repository.GetBasket(query.UserName);
+        return new GetBasketResult(basket);
     }
 }
--- src/Services/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -13,7 +13,8 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     }
 }
 
-public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+public class StoreBasketCommandHandler(IBasketRepository repository)
+    : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
@@ -21,6 +22,7 @@ public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, Sto
 
         // TODO: store basket in database (use Marten upsert- if exist = update, if not = insert)
         // TODO: update cache
-        return new StoreBasketResult("swn");
+        await repository.StoreBasket(command.Cart, cancellationToken);
+        return new StoreBasketResult(command.Cart.UserName);
     }
 }
