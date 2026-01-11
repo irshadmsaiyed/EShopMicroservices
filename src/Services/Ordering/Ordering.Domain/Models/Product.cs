f85fbe4264d04d51a6f476c1779b2a88491e7911
irshadmsaiyed <irshadmsaiyed@gmail.com>
2026-01-11 19:29:46 +0400

    developed Rich-domain model for order entities

--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -1,7 +1,21 @@
 namespace Ordering.Domain.Models;
 
-public class Product : Entity<Guid>
+public class Product : Entity<ProductId>
 {
-  public string Name { get; private set; } = default!;
-  public decimal Price { get; private set; } = default!;
+    public string Name { get; private set; } = default!;
+    public decimal Price { get; private set; } = default!;
+
+    public static Product Create(ProductId id, string name, string email, int productId, int quantity, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var product = new Product
+        {
+            Id = id,
+            Name = name,
+            Price = price
+        };
+        return product;
+    }
 }
